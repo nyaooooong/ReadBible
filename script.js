925c74537dafec335736651aef9a9f4e7193c233
jaeung.lee <jaeung.lee@lge.com>
2019-12-10 15:26:16 +0900

    add table header

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function loadDoc() {
+	setTableHeader();
+
+
 	var req = new XMLHttpRequest();
 	req.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -9,16 +12,34 @@ function loadDoc() {
 	req.send();
 }
 
+function setTableHeader() {
+	var header = document.createElement('th');
+	header.innerHTML = 'Day';
+	document.getElementById("table").appendChild(header);
+	header = document.createElement('th');
+	header.innerHTML = '4복음서';
+	document.getElementById("table").appendChild(header);
+	header = document.createElement('th');
+	header.innerHTML = '구약';
+	document.getElementById("table").appendChild(header);
+	header = document.createElement('th');
+	header.innerHTML = '시가서';
+	document.getElementById("table").appendChild(header);
+	header = document.createElement('th');
+	header.innerHTML = '신약';
+
+	document.getElementById("table").appendChild(header);
+}
+
 function myFunction(xml) {
 	var i, j;
 	var xmlDoc = xml.responseXML;
 
-	document.getElementById("table").appendChild('<th>Day</th><th>4복음서</th><th>구약</th><th>시가서</th><th>신약</th>');
 	var entries = xmlDoc.getElementsByTagName('Day');
 	for (i = 0; i < entries.length; i++) {
 		var day = entries[i].getAttribute('month') + '/' + entries[i].getAttribute('day');
-		var row = '<tr>';
-		row += '<td>' + day + '</td>';
+		var row = document.createElement('tr');
+		row.innerHTML = '<td>' + day + '</td>';
 		for (j = 0; j < 4; j++) {
 			var entry;
 			switch(j) {
@@ -38,9 +59,8 @@ function myFunction(xml) {
 			var book = entry.getElementsByTagName('book')[0].firstChild.data;
 			var chapter = entry.getElementsByTagName('chapter')[0].firstChild.data;
 			var verse = entry.getElementsByTagName('verse')[0].firstChild.data;
-			row += '<td>' + book + ' ' + chapter + ':' + verse + '</td>';
+			row.innerHTML += '<td>' + book + ' ' + chapter + ':' + verse + '</td>';
 		}
-		row += '</tr>';
 
 		document.getElementById("table").appendChild(row);
 	}
